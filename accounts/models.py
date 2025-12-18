1087044b4aecb20fab5b1ea5d03df1bb0863e52a
bdowsett <bendowsett@gmail.com>
2025-12-18 16:25:49 +0000

    user authentication

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null = True, blank = True)
+    email = models.EmailField(unique= True, blank= False, null = False)
+    
+    
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,66 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+
+class UsersManagersTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            first_name = "Chris",
+            username="testuser", 
+            email="testuser@example.com", 
+            password="testpass1234",
+            age = 46
+        )
+        self.assertEqual(user.first_name, "Chris")
+        self.assertEqual(user.username, "testuser")
+        self.assertEqual(user.email, "testuser@example.com")
+        self.assertEqual(user.age, 46)
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="testsuperuser",
+            email="testsuperuser@example.com",
+            password="testpass1234",
+            age =46,
+        )
+        self.assertEqual(admin_user.username, "testsuperuser")
+        self.assertEqual(admin_user.email, "testsuperuser@example.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        self.assertEqual(admin_user.age, 46)
+
+class SignupPageTests(TestCase):  
+    def test_url_exists_at_correct_location_signupview(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_name(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/signup.html")
+
+    def test_signup_form(self):
+        response = self.client.post(
+            reverse("signup"),
+            {
+                "username": "testuser",
+                "email": "testuser@email.com",
+                "password1": "testpass123",
+                "password2": "testpass123",
+                "age ": 34
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, "testuser")
+        self.assertEqual(
+          get_user_model().objects.all()[0].email, "testuser@email.com"
+        )
