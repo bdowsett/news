f19bbcb4b3a4d5e304c8eb66accb19a247bd5ff6
bdowsett <bendowsett@gmail.com>
2025-12-19 11:08:22 +0000

    Add Bootstrap stylin

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,7 +7,7 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        fields = ["username", "email"]
+        fields = ["username", "email", "age"]
 
 
 class CustomUserChangeForm(UserChangeForm):
